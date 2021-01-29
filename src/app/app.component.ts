15b48439711cd17c06ab594ceba193b0301257f8
Arnold <antaloczi.arnold@gmail.com>
2021-01-29 20:05:43 +0100

    Added product card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './model/product';
+import { ProductServiceService } from './service/product-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'str-angular-project001';
+  productList: Product[] = this.ProductServiceService.list;
+  constructor(private ProductServiceService: ProductServiceService) {}
 }
