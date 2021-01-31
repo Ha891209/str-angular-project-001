29acebdba71d8301ff1555bc1b2b26ae7c258788
Arnold <antaloczi.arnold@gmail.com>
2021-01-31 21:07:15 +0100

    Added design

--- src/app/common/product-list-featured-films/product-list-featured-films.component.ts
+++ src/app/common/product-list-featured-films/product-list-featured-films.component.ts
@@ -15,7 +15,9 @@ export class ProductListFeaturedFilmComponent implements OnInit {
   featuredList: Product[] = [...this.ProductServiceService.list]
     .filter(item => { return item.featured === true && item.catId === 1});
   
-  fiveFeaturedFilms: Product[] = this.featuredList.slice(0, 5);
+  fiveFeaturedFilms: Product[] = this.featuredList
+    .sort( () => 0.5 - Math.random())
+    .slice(0, 5);
 
   constructor(private ProductServiceService: ProductServiceService) { }
 
