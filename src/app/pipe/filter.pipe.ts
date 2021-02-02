e0acd3fa44a2919d7b40185fcb3553658964b0bd
Arnold <antaloczi.arnold@gmail.com>
2021-02-02 10:32:48 +0100

    Fixed filter pipe

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   
-  transform(value: any[], phrase: string, key: string = ''): any {
-    !Array.isArray(value) || !phrase || !key ? value : null;
-    return value.filter(item => String(item[key]).toLowerCase().includes(phrase.toLowerCase()) ? true : false);
+  transform(value: any[], key: string, phrase: string): any[] {
+
+    if (!Array.isArray(value) || !key || !phrase) {
+      return value;
+    }
+
+    return value.filter(item => {
+      
+      if (typeof item[key] === 'number' && typeof phrase === 'number') {
+        return item[key] === phrase;
+      }
+
+      phrase = ('' + phrase).toLowerCase();
+      return ('' + item[key]).toLowerCase().includes(phrase);
+    });
+
   }
 }
